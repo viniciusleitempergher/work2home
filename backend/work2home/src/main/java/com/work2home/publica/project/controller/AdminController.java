67bfacb1f344625bc657c593bc3d5bff715338cb
viniciusleitempergher <viniciusgabriel.com@gmail.com>
2022-04-27 15:06:42 -0300

    modelo denuncia feito, cadastrar admin feito

---
+++ backend/work2home/src/main/java/com/work2home/publica/project/controller/AdminController.java
@@ -0,0 +1,28 @@
+package com.work2home.publica.project.controller;
+
+import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.work2home.publica.project.dto.UsuarioDto;
+import com.work2home.publica.project.service.AdminService;
+
+@RolesAllowed("ROLES_ADMIN")
+@RestController("/admin")
+public class AdminController {
+
+	@Autowired
+	private AdminService adminService;
+	
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@PostMapping
+	public void createAdmin(@RequestBody @Valid UsuarioDto requestBody) {
+		adminService.cadastrar(requestBody);
+	}
+}
