9102cafd83f0498bc45a21052f738c79b6e64aec
viniciusleitempergher <viniciusgabriel.com@gmail.com>
2022-04-28 08:08:57 -0300

    denuncia corrigido

--- backend/work2home/src/main/java/com/work2home/publica/project/model/Denuncia.java
+++ backend/work2home/src/main/java/com/work2home/publica/project/model/Denuncia.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -13,6 +14,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "denuncia_tb")
+@IdClass(DenunciaId.class)
 public class Denuncia implements Serializable {
 
 	private static final long serialVersionUID = 6691799932343781245L;
