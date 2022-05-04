af9e032d812b75da2d029c9b055cf383c8f34764
viniciusleitempergher <viniciusgabriel.com@gmail.com>
2022-05-04 08:19:50 -0300

    importações corrigidas

--- frontend/src/app/screens/cliente/cliente-main-screen/cliente-main-screen.component.ts
+++ frontend/src/app/screens/cliente/cliente-main-screen/cliente-main-screen.component.ts
@@ -1,8 +1,9 @@
-import { Categoria } from './../../../models/Categoria';
-import { CategoriaService } from './../../services/categoria.service';
+
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CategoriaService } from 'src/app/services/categoria.service';
+import { Categoria } from 'src/models/Categoria';
 
 @Component({
   selector: 'app-cliente-main-screen',
